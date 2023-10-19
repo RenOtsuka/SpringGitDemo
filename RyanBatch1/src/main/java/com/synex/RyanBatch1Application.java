61b6fd1572dd93a0eed56b4a2f96c13ae67b45cd
Ryan <ryansantos.delossantos121@gmail.com>
2023-10-18 20:12:40 -0500

    first commit

---
+++ RyanBatch1/src/main/java/com/synex/RyanBatch1Application.java
@@ -0,0 +1,13 @@
+package com.synex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanBatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanBatch1Application.class, args);
+	}
+
+}
---
+++ RyanBatch1/src/test/java/com/synex/RyanBatch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanBatch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
